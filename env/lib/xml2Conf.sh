9b4adcff3b034f09e55b47cae42f2116fc0e1ebc
Roshas Johnson <roshasj10gmail.com>
2022-08-07 12:01:08 +0530

    initial commit

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/roshas/sample/env/lib"
+XML2_LIBS="-lxml2 -L/home/roshas/sample/env/lib -lz -L/home/roshas/sample/env/lib -llzma -lpthread -L/home/roshas/sample/env/lib -L/home/roshas/sample/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/roshas/sample/env/include/libxml2 -I/home/roshas/sample/env/include"
+MODULE_VERSION="xml2-2.9.14"
+
